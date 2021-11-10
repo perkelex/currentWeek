7a5d4e7cd72bb75e5fadcfd18fc45161534aa218
Orlando Mitrut <orlando.mitrut@gmail.com>
2021-11-10 20:17:26 +0200

    Updated init script

--- initialize.sh
+++ initialize.sh
@@ -1,19 +1,15 @@
 if [ ! -d  .venv ]; then
-    echo Creating python virtual environment .venv
+    echo "Creating python virtual environment .venv"
     python -m venv .venv
-    echo Virtual environment successfully created
 fi
 
-echo Activating virtual environment
+echo "Activating virtual environment"
 source .venv/Scripts/activate
 
-echo Updating .venv pip
+echo "Updating .venv pip"
 python -m pip install -U pip
 
-echo Installing project requirements in .venv
+echo "Installing project requirements in .venv"
 pip install -r requirements.txt
 
-# echo Deactivating .venv
-# deactivate
-
-echo Initialization done!
+echo "Initialization done!"
